ebf2c4df595b93e41a2668c5cbd921fb41431147
Thomas Witzer <thomas@tweezer.at>
2020-01-05 21:18:51 +0100

    fixed a bug from 0.2
    as a beginner in python I was using os.scandir() wrong. but now I was able to fix it with the help from a user from mastodon

--- IPTCrenamer.py
+++ IPTCrenamer.py
@@ -3,17 +3,18 @@ import os, sys, subprocess
 filecounter = 1
 if len(sys.argv) > 1:
    path = sys.argv[1]
-   files = os.scandir(path)
-   for filename in files:
-       if filename.is_file():
-           if ".jpg" in filename.name:
-             fullfilename = path + filename.name
-             exifreturn = subprocess.run(["exiftool","-Description",fullfilename],capture_output=True,text=True).stdout
-             exifarray = exifreturn.split(":")
-             newfilename = exifarray[1].strip() + ".jpg"
-             os.rename(fullfilename,path + newfilename)
-             print("Renaming File #" + str(filecounter) + ": " + filename.name + " -> " + newfilename)
-             filecounter += 1
+   filebuffer = []
+   for files in os.scandir(path):
+       if files.is_file() and files.name.endswith('.jpg'):
+          filebuffer.append(files.name)
+   for filename in filebuffer:
+       fullfilename = path + filename
+       exifreturn = subprocess.run(["exiftool","-Description",fullfilename],capture_output=True,text=True).stdout
+       exifarray = exifreturn.split(":")
+       newfilename = exifarray[1].strip() + ".jpg"
+       os.rename(fullfilename,path + newfilename)
+       print("Renaming File #" + str(filecounter) + ": " + filename + " -> " + newfilename)
+       filecounter += 1
 else:
     path("Path is missing! Please start the script this way: iptcrenamer.py /path/to/your/files/")
     exit()
